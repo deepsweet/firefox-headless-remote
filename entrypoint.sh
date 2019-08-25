f3e6d013da744657f8349c8a52dd158f56126545
Bar Hofesh <bar.hofesh@gmail.com>
2019-08-25 20:04:38 +0300

    Add an option to set env var for port bind

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,6 @@ fi
 
 ip=$(hostname --ip-address)
 
-socat tcp-listen:2828,bind="$ip",fork tcp:127.0.0.1:2828 &
+socat tcp-listen:$FIREFOX_BIND_PORT,bind="$ip",fork tcp:127.0.0.1:2828 &
 
-/usr/bin/firefox -headless -marionette -safe-mode -profile /home/firefox/profile/
+/usr/bin/firefox -headless -marionette -profile /home/firefox/profile/
